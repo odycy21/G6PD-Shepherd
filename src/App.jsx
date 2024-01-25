1c1abf497de46bdfb3c16186176a646b1a81df50
bryanosaldomain <102651083+bryanosaldomain@users.noreply.github.com>
2024-01-26 00:42:10 +0800

    added search input and background color

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import { useState } from 'react'
-import Navbar from './Navbar'
-
+import React from "react";
+import Navbar from "./Navbar";
+import './index.css';
+import data from "./db.json";
 
 function App() {
 
   return (
-    <>
-        <Navbar/>
+
+    <> 
+      <div className="main-container">
+            <div className="search-container">
+                <input type="text" id="searchInput" placeholder="Input food or medicine name..."/>
+            </div>
+      </div>
+
+      <div className="item-container">
+
+      </div>
+    
     </>
   )
 }
