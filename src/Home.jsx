a471367dc61302f5da11f980326c1a5486606092
bryanosaldomain <102651083+bryanosaldomain@users.noreply.github.com>
2024-03-08 03:54:23 +0800

    install axios, updated search bar width

--- src/Home.jsx
+++ src/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
         <div className="max-w-[1000px] mt-[-5px] py-10  h-screen mx-auto  justify-center items-center">
            
             <div className="p-20 lg:py-5 px-10 py-10">
-                <h1 className='font-montserrat font-semibold text-xl pb-2'> What is G6PD? </h1>
+                <h1 className='font-montserrat font-semibold text-xl pb-2'> What is G6PD? sss</h1>
                 <p>
                     G6PD deficiency is a genetic disorder that most often affects males. It happens when the body doesn't have enough of an enzyme called glucose-6-phosphate dehydrogenase (G6PD).
                     G6PD helps red blood cells work. It also protects them from substances in the blood that could harm them. In people with G6PD deficiency, either the red blood cells do not make enough G6PD or what they do make doesn't work as it should. <NavLink to="/about" className="linkColor"> Learn more about G6PD.</NavLink>
