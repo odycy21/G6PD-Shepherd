1bbdf78e7444caa3bc3c3b3b4e247938aab2041e
bryanosaldomain <102651083+bryanosaldomain@users.noreply.github.com>
2024-03-08 04:08:10 +0800

    added content component

--- src/Home.jsx
+++ src/Home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
             </div>
 
 
-            <div className='flex justify-center py-5'>
+            <div className='flex justify-center py-5 max-w-[1000px]'>
                  <Content/>
             </div>
 
