b929987e42e326bd6f0fe3f8a93ca2746bb5590c
bryanosaldomain <102651083+bryanosaldomain@users.noreply.github.com>
2024-03-10 22:59:38 +0800

    updated notes on home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
         <div className="max-w-[1000px] py-20 mx-auto my-auto justify-center items-center">
            
             <div className="p-20 lg:py-5 px-10 py-10">
-                <p className='font-montserrat font-semibold text-xl pb-2'> What is G6PD?</p>
+                <h1 className='font-montserrat font-semibold text-xl pb-2'> What is G6PD?</h1>
                 <p>
                     G6PD deficiency is a genetic disorder that most often affects males. It happens when the body doesn't have enough of an enzyme called glucose-6-phosphate dehydrogenase (G6PD).
                     G6PD helps red blood cells work. It also protects them from substances in the blood that could harm them. In people with G6PD deficiency, either the red blood cells do not make enough G6PD or what they do make doesn't work as it should. 
@@ -19,7 +19,7 @@ const Home = () => {
                    
                     <br /> <br />
                     
-                    <p> <b>Note:</b> Type the name of food, drug, medicine or chemical below to check if it's safe for G6PD patients. </p>
+                    <p> <b>Note:</b> Type the name of food, drug, medicine or chemical and if you see them below, it is not safe for G6PD patients. </p>
 
                     <div className='my-[50px] flex justify-center'>
                 
