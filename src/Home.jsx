2c73c0bcb1cc537405164974429dd80220a7a972
bryanosaldomain <102651083+bryanosaldomain@users.noreply.github.com>
2024-03-10 15:33:48 +0800

    updated home page

--- src/Home.jsx
+++ src/Home.jsx
@@ -17,13 +17,11 @@ const Home = () => {
                 <p>
                     G6PD deficiency is a genetic disorder that most often affects males. It happens when the body doesn't have enough of an enzyme called glucose-6-phosphate dehydrogenase (G6PD).
                     G6PD helps red blood cells work. It also protects them from substances in the blood that could harm them. In people with G6PD deficiency, either the red blood cells do not make enough G6PD or what they do make doesn't work as it should. 
-                    <br /> <br />
-                    When many red blood cells are destroyed, a person can develop hemolytic (hee-meh-LIH-tik) anemia. This can cause tiredness, dizziness, and other symptoms. 
-                
                     <NavLink to="/about" className="linkColor"> Learn more about G6PD.</NavLink>
+                   
                     <br /> <br />
                     
-                    <p> Note: Type the name of food or medicine below to check if it's safe for G6PD patients. </p>
+                    <p> <b>Note:</b> Type the name of food or medicine below to check if it's safe for G6PD patients. </p>
 
                     <div className='flex justify-center'>
                         <Searchbar/>
