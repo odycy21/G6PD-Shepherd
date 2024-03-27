f00d99f304525d2ced8d64a7b5954f1e4164cc5e
bryanosaldomain <102651083+bryanosaldomain@users.noreply.github.com>
2024-03-28 00:51:33 +0800

    edited contact us page

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -6,7 +6,7 @@ const ContactUs = () => {
     <div className="max-w-[1000px] min-h-screen mt-[-5px] py-20  mx-auto flex flex-col justify-center items-center">
            
         <div className="p-20 lg:py-5 px-10 py-10">
-              <p className='font-montserrat font-semibold text-xl pb-2'> Do You have Questions or Suggestions? </p>
+              <p className='font-montserrat font-semibold text-xl pb-2'> Do You have Questions? </p>
                <p className="py-2 text-center">
                    Email us @ bryanosal@gmail.com 
                </p>
