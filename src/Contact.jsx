d98f3e426b410d82891de02d9b2786214340a486
bryanosaldomain <102651083+bryanosaldomain@users.noreply.github.com>
2024-04-08 03:32:06 +0800

    updated contact page

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,17 +1,78 @@
-import { NavLink } from "react-router-dom";
+
+import React, {useRef} from 'react';
+import emailjs from '@emailjs/browser';
 
 const ContactUs = () => {
+
+  const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        const fullName = document.querySelector("#fullName").value;
+        const email = document.querySelector("#email").value;
+        const message = document.querySelector("#message").value;
+
+
+        if(fullName.length === 0){
+            alert("Please Input something. ..");
+            return false;
+        }else if(email.length === 0){
+            alert("Please Input something. ..");
+            return false;
+        }else if(message.length === 0){
+            alert("Please Input something. ..");
+            return false;
+        }else{
+
+        emailjs.sendForm('service_k0qdrpq', 'template_3788e6p', form.current, 's4wwLmtU6tc-DJo7N')
+          .then((result) => {
+              console.log(result.text);
+              alert("Message Sent!");
+              form.current.reset();
+          }, (error) => {
+              console.log(error.text);
+          });
+        }
+      };
+
+  
     return ( 
 
-    <div className="max-w-[1000px] min-h-screen mt-[-5px] py-20  mx-auto flex flex-col justify-center items-center">
+      <div className="max-w-[1240px] w-full lg:h-screen md:h-screen mx-auto grid py-[15rem] px-4 bg-transparent text-center">
            
-        <div className="p-20 lg:py-5 px-10 py-10">
-              <p className='font-montserrat font-semibold text-xl pb-2'> Do You have Questions? </p>
-               <p className="py-2 text-center">
-                   Email us @ bryanosal@gmail.com 
-               </p>
-       </div>
-    </div>   
+      <div>
+              <h2 className='font-montserrat font-semibold text-[30px] pb-2'>Get In Touch with us!</h2>
+
+              <p className="text-sm leading-7 pb-10">
+                Let us know so we can improve our services.<br />
+              </p>
+
+           
+              <div className='max-w-[680px] mx-auto'>
+                  <form ref={form} onSubmit={sendEmail}>
+                  <label>Full Name</label>
+                  <input type="text" name="user_name" id='fullName'/>
+                  <label>Email Address</label>
+                  <input type="email" name="user_email" id='email' />
+                  <label>Message</label>
+                  <textarea name="message" id='message'/>
+                  <input type="submit" value="Send" className="mt-5 w-[20%] h-[32px] border border-b-4 border-white rounded-sm font-normal text-white text-center text-xs hover:bg-[#733f1db3]" />
+                  </form>
+              </div>
+      </div>
+
+      
+      <div>
+
+
+      </div>
+      
+
+     </div> 
+
+      
+  
 
      );
 }
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,14 +1,18 @@
-import React from "react";
 
+import React from "react";
+import {  NavLink } from "react-router-dom";
 
 const Footer = () => {
 
     let currentDate =  new Date().getFullYear();
 
     return ( 
+
+        <div className="leading-7">
             <div className="footer bottom-0 text-center my-auto text-xs pl-8 pr-8 pb-8 font-semi-bold">
-             ©{currentDate} - Develop by Bryan Osal
+             ©{currentDate} All Rights Reserved - G6PD Shepherd <br />Develop by <NavLink className = "linkColor2" to={"https://bryan-dev.vercel.app/"}>Bryan Osal</NavLink> 
             </div>
+        </div>
      );
 }
  
